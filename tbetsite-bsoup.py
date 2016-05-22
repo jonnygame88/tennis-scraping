21e2fcb72a86392e118de4b3690472ff12644478
jonnygame88 <snows_88@hotmail.com>
2016-05-22 22:35:18 +0100

    Update tbetsite-bsoup.py

--- tbetsite-bsoup.py
+++ tbetsite-bsoup.py
@@ -1,5 +1,11 @@
+## match list: list year urls on player page, get matches for each url
+## player list: add opponent if opponent not in player list
+## player1 > get match list > format and append match data, add to player list
+## repeat for players in player list
+
 from bs4 import BeautifulSoup
 import urllib.request as urlr
+import datetime
 import pandas as pd
 pd.set_option('display.width', 1000)
 
@@ -15,29 +21,35 @@ for link in soup_player.find_all('a'):
 
 ## get match info
 def match_info(soup_match):
-    s = soup_match.findAll('div', attrs = {'class': 'block'})[0]
-    date = s.findAll('div', attrs = {'class': 'calendar'})[0].get_text().replace('\n', ' ')
-    tourn = s.h2.get_text()
-    surface = s.findAll('span', attrs = {'class': 'maininfo'})[0].get_text()
-    round = s.findAll('td', attrs = {'class': 'round'})[0].get_text()
-    players = [x.get_text() for x in s.findAll('acronym', {"title": True})]
-    sets = [x.get_text() for x in s.findAll('td', attrs = {'class': 'set'})]
-    return(date, tourn, surface, round, 
-           [x for x in players if len(x)>0], [x for x in sets if len(x)>0])
+    tourn = soup_match.h2.get_text()
+    surface = soup_match.findAll('span', attrs = {'class': 'maininfo'})[0].get_text()
+    surface = surface[0:surface.find(',')]
+    date = soup_match.findAll('div', attrs = {'class': 'calendar'})[0].get_text()
+    date = date.replace('\n', ' ').replace(' ', '')
+    soup_scores = soup_match.findAll('table', attrs = {'class': 'scores'})[0]
+    round = soup_scores.findAll('td', attrs = {'class': 'round'})[0].get_text()
+    players = [x.get_text() for x in soup_scores.findAll('acronym', {"title": True})]
+    sets = [x.get_text() for x in soup_scores.findAll('td', attrs = {'class': 'set'})]
+    return([date, tourn, surface, round, players, sets])
 
 ## get match stats
 def match_stats(soup_match):
     gs = soup_match.findAll('table', attrs = {'class': 'gamestats'})[0].tbody
-    gs_title = gs.findAll(attrs = {'class': 'title'})
-    gs_val = gs.findAll(attrs = {'class': 'value'})
-    t_metrics = [i.get_text() for i in gs_title]
-    t_vals = [i.get_text() for i in gs_val]
-    t_entries = [[t_metrics[i], t_vals[2*i], t_vals[2*i + 1]] for i in range(14)]
-    return(pd.DataFrame(t_entries, columns = ['metric', 'player1', 'player2']))
+    if gs is None:
+        return([])
+    else:
+        gs_title = gs.findAll(attrs = {'class': 'title'})
+        gs_val = gs.findAll(attrs = {'class': 'value'})
+        t_metrics = [i.get_text() for i in gs_title]
+        t_vals = [i.get_text() for i in gs_val]
+        t_entries = [[t_metrics[i], t_vals[2*i], t_vals[2*i + 1]] for i in range(14)]
+        return(pd.DataFrame(t_entries, columns = ['metric', 'player1', 'player2']))
 
 ## output
+tstart = datetime.datetime.now()
 for n in range(len(match_links)):
     url_match = match_links[n]
     page_match = urlr.urlopen(url_match)
     s = BeautifulSoup(page_match.read(), "lxml")
     print(match_info(s), '\n\n', match_stats(s), '\n')
+print(len(match_links), tstart, datetime.datetime.now())
