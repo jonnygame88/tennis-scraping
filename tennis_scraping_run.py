d822063e3b2b1535707b6e166803656ac78ec19a
jonnygame88 <snows_88@hotmail.com>
2016-06-04 16:29:51 +0100

    Update tennis_scraping_run.py

--- tennis_scraping_run.py
+++ tennis_scraping_run.py
@@ -5,10 +5,10 @@ pd.set_option("display.max_rows", 250)
 
 data_path = 'C:\\Projects\\tennis-modelling\\data\\player-csvs\\'
 opponent_ids = pd.read_csv(data_path + 'opponent-ids-atp.csv').opponent_id
-completed, to_complete = 2, 10
+errors, start_loc, end_loc = [], 9, 20
 gender, yr1, yr2 = 'm', 2006, 2016
 
-for i in list(range(completed, to_complete)):
+for i in list(range(start_loc, end_loc+1)):
     player_name = get_name(opponent_ids[i], gender)
     try:
         print(player_name)
@@ -18,6 +18,7 @@ for i in list(range(completed, to_complete)):
             print(player_name + str('-2'))
             player_csv(player_name + str('-2'), gender, yr1, yr2)
         except Exception:
+            errors.append(player_name)
             pass
 
 ## test: jimmy wang, john isner
